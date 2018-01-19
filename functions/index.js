94c97e252cf2be77999f54e09f6c304c08228f61
Kevin Coyner <rustybear@gmail.com>
2018-01-19 13:03:24 -0500

    Lint cleanup with standard.

--- functions/index.js
+++ functions/index.js
@@ -6,9 +6,9 @@
 // # firebase function cli commands for deploy
 // firebase deploy --only functions
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
 exports.sendMessage = functions.database
   .ref('/gfdDispatches/{pushId}')
@@ -19,28 +19,28 @@ exports.sendMessage = functions.database
     // console.log('dispatchObj', dispatchObj);
 
     // get pushed id
-    const pushID = event.params.pushId;
-    console.log('event.params.pushId', event.params.pushId);
+    const pushID = event.params.pushId
+    console.log('event.params.pushId', event.params.pushId)
 
     // -- start twilio SMS text sending code --
     // twilio access info
-    const twilio_account_sid = functions.config().ers.twilio_account_sid;
-    const twilio_auth_token = functions.config().ers.twilio_auth_token;
+    const twilio_account_sid = functions.config().ers.twilio_account_sid
+    const twilio_auth_token = functions.config().ers.twilio_auth_token
     // twilio phone number
-    const TWILIO_PHONE_NUMBER = +18055902198;
+    const TWILIO_PHONE_NUMBER = +18055902198
     // member's phone number
-    const masato_phone_number = functions.config().ers.masato;
-    const kevin_phone_number = functions.config().ers.kevin;
-    const nick_phone_number = functions.config().ers.nick;
-    const max_phone_number = functions.config().ers.max;
+    const masato_phone_number = functions.config().ers.masato
+    const kevin_phone_number = functions.config().ers.kevin
+    const nick_phone_number = functions.config().ers.nick
+    const max_phone_number = functions.config().ers.max
     const phoneNumbers = [
       masato_phone_number,
       kevin_phone_number
-    ];
+    ]
 
-    //require the Twilio module and create a REST client
-    const client = require('twilio')(twilio_account_sid, twilio_auth_token);
-    const sms_message = `https://ers-dispatch.firebaseapp.com/?id=${pushID}`;
+    // require the Twilio module and create a REST client
+    const client = require('twilio')(twilio_account_sid, twilio_auth_token)
+    const sms_message = `https://ers-dispatch.firebaseapp.com/?id=${pushID}`
     for (let phoneNumber of phoneNumbers) {
       client.messages.create(
         {
@@ -48,9 +48,9 @@ exports.sendMessage = functions.database
           to: phoneNumber,
           body: sms_message
         },
-        function(err, message) {
-          console.log(err, message);
+        function (err, message) {
+          console.log(err, message)
         }
-      );
+      )
     }
-  });
+  })
